50195d07dc1210573860e40900f59fb1810d9106
Eduardo Zirbell <eduardozirbell26@gmail.com>
2022-05-17 21:13:09 -0300

    JS feito

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const personagens = document.querySelectorAll('.personagem')
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const idSelecionado = personagem.attributes.id.value
+        if (idSelecionado === 'ultron') return
+        // troca de brilho de personagens pra personagens
+        const personagemSelecionado = document.querySelector('.selecionado')
+        personagemSelecionado.classList.remove('selecionado')
+        personagem.classList.add('selecionado')
+
+        // troca de personagens grande
+        const personagemJogador1 = document.querySelector('#personagem-jogador-1')
+        personagemJogador1.src = `./src/imagens/${idSelecionado}.png`
+
+        // troca de nomes embaixo do personagem grande
+        const nomeJogador1 = document.querySelector('#nome-jogador-1')
+        const nomeSelecionado = personagem.getAttribute('data-name')
+        nomeJogador1.innerHTML = nomeSelecionado
+        
+    })
+})
